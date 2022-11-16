93117f027ee65fa31a1f0a7d8c57f3df253ab65b
erlendjoa <102193887+erlendjoa@users.noreply.github.com>
2022-11-16 14:05:03 +0100

    Update floating_box.py

--- floating_box.py
+++ floating_box.py
@@ -11,62 +11,88 @@ FPS = 60
 
 PLAYER_HIT = pygame.USEREVENT + 1
 
-PLAYER_HEALTH = 100
-ENEMY_HEALTH = 50
+class Player():
+    def __init__(self, health):
+        self._health = health
+        self._original_health = self._health
 
-BOX1 = pygame.Rect(150, HEIGHT//3, 100, 100)
-BOX2 = pygame.Rect(WIDTH-250, HEIGHT//3, 100, 100)
-FONT = pygame.font.SysFont("helvetica", 18)
-FLOATING_TEXT1 = FONT.render("Player",1,(0,0,0))
-FLOATING_TEXT2 = FONT.render("Enemy",1,(0,0,0))
-
-HEALTH_BAR_1 = pygame.Rect(BOX1.x-BOX1.width//5, BOX1.y+BOX1.height+40, 200, 20)
-HEALTH_BAR_2 = pygame.Rect(BOX2.x-BOX2.width//5, BOX2.y+BOX2.height+40, 200, 20)
-PLAYER_HEALTH_TEXT = FONT.render(str(PLAYER_HEALTH),1,(255,255,255))
-ENEMY_HEALTH_TEXT = FONT.render(str(ENEMY_HEALTH),1,(255,255,255))
-
-
-def draw_window():
-    WIN.fill((255, 255, 205))
-    rectpros()
-    blitpros()
-    pygame.display.update()
-
-def rectpros():
-    pygame.draw.rect(WIN, (0,255,0), BOX1)
-    pygame.draw.rect(WIN, (0,0,255), BOX2)
-    pygame.draw.rect(WIN, (255,0,0), HEALTH_BAR_1)
-    pygame.draw.rect(WIN, (255,0,0), HEALTH_BAR_2)
-def blitpros():
-    WIN.blit(FLOATING_TEXT1, (BOX1.x, BOX1.y+BOX1.height+10))
-    WIN.blit(FLOATING_TEXT2, (BOX2.x, BOX2.y+BOX2.height+10))
-    WIN.blit(PLAYER_HEALTH_TEXT, (HEALTH_BAR_1.x+5, HEALTH_BAR_1.y))
-    WIN.blit(ENEMY_HEALTH_TEXT, (HEALTH_BAR_2.x+5, HEALTH_BAR_2.y))
+    def get_health(self):
+        return self._health
+    
+    def change_health(self, damage):
+        while self._health > self._original_health - damage:
+            self._health -= 1
+            pygame.time.delay(50)
+        self._original_health = self._health
+        return self._health
 
 
-def change_health(HEALTH):
-        HEALTH -= 1
-        print(HEALTH)
+class Enemy():
+    def __init__(self, health):
+        self._health = health
+    def create_font(self, font):
+        ENEMY_HEALTH_TEXT = font.render(str(self._health),1,(255,255,255))
+        return ENEMY_HEALTH_TEXT
+    def create_rect(self, rect):
+        HEALTH_BAR_2 = pygame.Rect(rect.x-rect.width//5, rect.y+rect.height+40, 200, 20)
+        return HEALTH_BAR_2
 
-def main():
-    run = True
-    while run:
-        pygame.time.Clock().tick(FPS)
-        keys_pressed = pygame.key.get_pressed()
-        for event in pygame.event.get():
-            if event.type == pygame.quit:
-                run = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LCTRL:
-                    pygame.event.post(pygame.event.Event(PLAYER_HIT))
 
-            if event.type == PLAYER_HIT:
-                change_health(PLAYER_HEALTH)
-        
+
+class Window():
+    def __init__(self):
+        self._player = Player(100)
+        self._enemy = Enemy(80)
+
+        self._BOX1 = pygame.Rect(150, HEIGHT//3, 100, 100)
+        self._BOX2 = pygame.Rect(WIDTH-250, HEIGHT//3, 100, 100)
+        self._FONT = pygame.font.SysFont("helvetica", 18)
+
+        self._FLOATING_TEXT1 = self._FONT.render("Player",1,(0,0,0))
+        self._FLOATING_TEXT2 = self._FONT.render("Enemy",1,(0,0,0))
+        self._PLAYER_HEALTH_TEXT = self._FONT.render(str(self._player._health),1,(255,255,255))
+        self._ENEMY_HEALTH_TEXT = self._FONT.render(str(self._enemy._health),1,(255,255,255))
+        self._HEALTH_BAR_1 = pygame.Rect(self._BOX1.x-self._BOX1.width//5, self._BOX1.y+self._BOX1.height+40, 200, 20)
+        self._HEALTH_BAR_2 = pygame.Rect(self._BOX2.x-self._BOX2.width//5, self._BOX2.y+self._BOX2.height+40, 200, 20)
+
+    def draw_window(self):
+        WIN.fill((255, 255, 205))
+        self.rectpros()
+        self.blitpros()
+        pygame.display.update()
+
+    def rectpros(self):
+        pygame.draw.rect(WIN, (0,255,0), self._BOX1)
+        pygame.draw.rect(WIN, (0,0,255), self._BOX2)
+        pygame.draw.rect(WIN, (255,0,0), self._HEALTH_BAR_1)
+        pygame.draw.rect(WIN, (255,0,0), self._HEALTH_BAR_2)
+    def blitpros(self):
+        WIN.blit(self._FLOATING_TEXT1, (self._BOX1.x, self._BOX1.y+self._BOX1.height+10))
+        WIN.blit(self._FLOATING_TEXT2, (self._BOX2.x, self._BOX2.y+self._BOX2.height+10))
+        WIN.blit(self._PLAYER_HEALTH_TEXT, (self._HEALTH_BAR_1.x+5, self._HEALTH_BAR_1.y))
+        WIN.blit(self._ENEMY_HEALTH_TEXT, (self._HEALTH_BAR_2.x+5, self._HEALTH_BAR_2.y))
+
+    def keys_pressed(self, keys_pressed):
         if keys_pressed[pygame.K_5]:
             pygame.event.post(pygame.event.Event(PLAYER_HIT))
 
-        draw_window()
-    pygame.quit()
-if __name__ == "__main__":
-    main()
+    def main(self):
+        run = True
+        while run:
+            pygame.time.Clock().tick(FPS)
+            keys_pressed = pygame.key.get_pressed()
+            for event in pygame.event.get():
+                if event.type == pygame.quit:
+                    run = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LCTRL:
+                        pass
+                if event.type == PLAYER_HIT:
+                    self._player._health = self._player.change_health(10)
+
+            self.keys_pressed(keys_pressed)
+            self.draw_window()
+
+        pygame.quit()
+win = Window()
+win.main()
