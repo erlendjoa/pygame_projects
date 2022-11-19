de14303ab5c83f41e0a9854eedc0e51509e10e86
erlendjoa <102193887+erlendjoa@users.noreply.github.com>
2022-11-19 13:57:51 +0100

    Update best_game_ever_made.py

--- best_game_ever_made.py
+++ best_game_ever_made.py
@@ -1,75 +1,92 @@
 import pygame
 import os
-from random import randint
 from color_class import Colorcheck
-from action_classes import Player
-from action_classes import Enemy
-
+from preset import Preset
 pygame.font.init()
 pygame.mixer.init()
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Floating Box")
+pygame.display.set_caption("Kung Fu Panda 4")
 FPS = 60
 
 PLAYER_HIT = pygame.USEREVENT + 1
+ENEMY_HIT = pygame.USEREVENT + 2
+MOVE_1 = pygame.USEREVENT + 3
+CHANGE_CHAR1 = pygame.USEREVENT + 3
+CHANGE_CHAR2 = pygame.USEREVENT + 4
 
-class Window():
-    def __init__(self):
+class BattleWindow():
+    def __init__(self, ref, player, enemy):
+        self._ref = pygame.image.load(os.path.join(ref[0],ref[1],ref[2]))
+        self._img = pygame.transform.rotate(pygame.transform.scale(self._ref, (WIDTH, HEIGHT)), 0)
+        self._player = player
+        self._enemy = enemy
         self._colorcheck = Colorcheck()
-        self._player = Player("Po", 100)
-        self._player.set_move("Rock Smash", "ground", 25)
-        self._player.set_move("Psychic Beam", "psychic", 35)
-        self._enemy = Enemy("Tai Lung", 80)
-
-        self._BOX1 = pygame.Rect(100, HEIGHT//3, 200, 100)
-        self._BOX2 = pygame.Rect(WIDTH-250, HEIGHT//3, 200, 100)
-        self._FONT = pygame.font.SysFont("helvetica", 18)
-
-        self._HEALTH_BAR_1 = pygame.Rect(self._BOX1.x-self._BOX1.width//5, self._BOX1.y+self._BOX1.height+40, 200, 20)
-        self._HEALTH_BAR_2 = pygame.Rect(self._BOX2.x-self._BOX2.width//5, self._BOX2.y+self._BOX2.height+40, 200, 20)
-
-        self._ACTION_BAR = pygame.Rect(WIDTH//2-250, HEIGHT*0.75, 500, 125)
-        self._ACTION_MOVES = [pygame.Rect(self._ACTION_BAR.x+20, self._ACTION_BAR.y+self._ACTION_BAR.height//10, self._ACTION_BAR.width*0.42, 50), pygame.Rect(self._ACTION_BAR.x+self._ACTION_BAR.width//2+20, self._ACTION_BAR.y+self._ACTION_BAR.height//10, self._ACTION_BAR.width*0.42, 50), pygame.Rect(self._ACTION_BAR.x+20, self._ACTION_BAR.y+self._ACTION_BAR.height//10*6, self._ACTION_BAR.width*0.42, 50), pygame.Rect(self._ACTION_BAR.x+self._ACTION_BAR.width//2+20, self._ACTION_BAR.y+self._ACTION_BAR.height//10*6, self._ACTION_BAR.width*0.42, 50)]
+        self._FONT = pygame.font.SysFont("Helvetica", 18)
+        self._ACTION_BAR = pygame.Rect(WIDTH//2-250, HEIGHT*0.75, 400, 125)
+        self._ACTION_MOVES = self._player.load_moves(self._ACTION_BAR)
 
     def draw_window(self):
-        WIN.fill((220,220,220))
+        WIN.fill((208,218,220))
         self.rectpros()
         self.blitpros()
         pygame.display.update()
 
     def rectpros(self):
-        pygame.draw.rect(WIN, (255,0,0), self._HEALTH_BAR_1)
-        pygame.draw.rect(WIN, (255,0,0), self._HEALTH_BAR_2)
+        pygame.draw.rect(WIN, (0,0,0), self._player._blackbar)
+        pygame.draw.rect(WIN, (0,0,0), self._enemy._blackbar)
+        pygame.draw.rect(WIN, (255,0,0), self._player._healthbar)
+        pygame.draw.rect(WIN, (255,0,0), self._enemy._healthbar)
         self.draw_moves()
     def blitpros(self):
         self._FLOATING_TEXT1 = self._FONT.render(self._player._name,1,(0,0,0))
         self._FLOATING_TEXT2 = self._FONT.render(self._enemy._name,1,(0,0,0))
-        self._PLAYER_HEALTH_TEXT = self._FONT.render(str(self._player._health),1,(255,255,255))
+        self._PLAYER_HEALTH_TEXT = self._FONT.render(str(self._player._health),1,(255,255,255)) 
         self._ENEMY_HEALTH_TEXT = self._FONT.render(str(self._enemy._health),1,(255,255,255))
-        WIN.blit(self._player._img, (self._BOX1.x, self._BOX1.y-50))
-        WIN.blit(self._enemy._img, (self._BOX2.x-50, self._BOX2.y-50))
-        WIN.blit(self._FLOATING_TEXT1, (self._BOX1.x, self._BOX1.y+self._BOX1.height+10))
-        WIN.blit(self._FLOATING_TEXT2, (self._BOX2.x, self._BOX2.y+self._BOX2.height+10))
-        WIN.blit(self._PLAYER_HEALTH_TEXT, (self._HEALTH_BAR_1.x+5, self._HEALTH_BAR_1.y))
-        WIN.blit(self._ENEMY_HEALTH_TEXT, (self._HEALTH_BAR_2.x+5, self._HEALTH_BAR_2.y))
+        WIN.blit(self._player._img, (self._player._sprite.x, self._player._sprite.y-50))
+        WIN.blit(self._enemy._img, (self._enemy._sprite.x-50, self._enemy._sprite.y-50))
+        WIN.blit(self._FLOATING_TEXT1, (self._player._healthbar.x, self._player._healthbar.y+self._player._healthbar.height+10))
+        WIN.blit(self._FLOATING_TEXT2, (self._enemy._healthbar.x, self._enemy._healthbar.y+self._enemy._healthbar.height+10))
+        WIN.blit(self._PLAYER_HEALTH_TEXT, (self._player._healthbar.x+5, self._player._healthbar.y))
+        WIN.blit(self._ENEMY_HEALTH_TEXT, (self._enemy._healthbar.x+5, self._enemy._healthbar.y))
+        
     def draw_moves(self):
-        pygame.draw.rect(WIN, (0,0,0), self._ACTION_BAR)
+        pygame.draw.rect(WIN, (165,184,187), self._ACTION_BAR)
         for i in range(len(self._player._moves)):
-            if self._player._moves[i] != None:
-                pygame.draw.rect(WIN, self._colorcheck.get_color(self._player._moves[i][1]), self._ACTION_MOVES[i])
-                text = self._FONT.render(str(self._player._moves[i][0]),1,(0,0,0))
-                WIN.blit(text, (self._ACTION_MOVES[i].x, self._ACTION_MOVES[i].y))
+            self._player._ACTION_MOVES[i].click()
+            self._player._ACTION_MOVES[i].draw(WIN)
+
+    def combatpros(self):
+        if self._player._usedmove != None:
+            print(self._player._usedmove)
+            self._player._usedmove = None
 
     def keys_pressed(self, keys_pressed):
         if keys_pressed[pygame.K_5]:
+            pygame.event.post(pygame.event.Event(CHANGE_CHAR1))
+        if keys_pressed[pygame.K_6]:
+            pygame.event.post(pygame.event.Event(CHANGE_CHAR2))
+        if keys_pressed[pygame.K_1]:
             pygame.event.post(pygame.event.Event(PLAYER_HIT))
+        if keys_pressed[pygame.K_2]:
+            pygame.event.post(pygame.event.Event(ENEMY_HIT))
     def eventpros(self, event):
+            if event.type == pygame.MOUSEBUTTONUP:
+                pygame.event.post(pygame.event.Event(PLAYER_HIT))
             if event.type == PLAYER_HIT:
-                self._player._health -= self._enemy.move_1()
-                if self._player._health > 0:
-                    self._HEALTH_BAR_1.width -= self._HEALTH_BAR_1.width//self._player._health*self._enemy.move_1()
+                self._player._health -= self._enemy._moves[0]._dmg
+                self._player._healthbar.width -= self._player._healthbar.width//self._enemy._moves[0]._dmg
+            if event.type == ENEMY_HIT:
+                self._enemy._health -= self._player._moves[0]._dmg
+            if event.type == MOVE_1:
+                pass
+            if event.type == CHANGE_CHAR1:
+                self._player = preset.get_Po(True)
+                self._ACTION_MOVES = self._player.load_moves(self._ACTION_BAR)
+            if event.type == CHANGE_CHAR2:
+                self._player = preset.get_Shifu(True)
+                self._ACTION_MOVES = self._player.load_moves(self._ACTION_BAR)
         
 
     def main(self):
@@ -81,10 +98,14 @@ class Window():
                 if event.type == pygame.quit:
                     return False
                 self.eventpros(event)
-
             self.keys_pressed(keys_pressed)
+            
+            self.combatpros()
 
             self.draw_window()
         pygame.quit()
-win = Window()
+
+
+preset = Preset()
+win = BattleWindow(["xprojekt1","Assets","scenery.jpg"], preset.get_TaiLung(True), preset.get_Po(False))
 win.main()
