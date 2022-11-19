986ac6ac1c95710e0d009baff72622706b95e5a1
erlendjoa <102193887+erlendjoa@users.noreply.github.com>
2022-11-19 16:55:38 +0100

    Update move.py

--- move.py
+++ move.py
@@ -1,8 +1,9 @@
 import pygame
 import os
-from color_class import Colorcheck
+from check import Colorcheck, Movecheck
 
 COLORCHECK = Colorcheck()
+MOVECHECK = Movecheck()
 
 class Move():
     def __init__(self, name, type, dmg, val):
@@ -11,3 +12,11 @@ class Move():
         self._color = COLORCHECK.get_color(type)
         self._dmg = dmg
         self._val = val
+    
+    def effectiveness(self, opponent):
+        if MOVECHECK.check(self, opponent) == None:
+            return self._dmg
+        if MOVECHECK.check(self, opponent):
+            return self._dmg + self._dmg//2
+        if MOVECHECK.check(self, opponent) == False:
+            return self._dmg - self._dmg//2
